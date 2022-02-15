b03442004c63b444fc735fec819d39d304519f9b
rkorotkiy <93907071+rkorotkiy@users.noreply.github.com>
2022-02-15 17:08:33 +0300

    Update Source.cpp

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -24,11 +24,14 @@ public:
 	void Initialize(void*);  //   ???
 	void Finalize();
 
-	void OpenSession(CK_SLOT_ID slotID, CK_SESSION_HANDLE h_session);
-	void CloseSession(CK_SESSION_HANDLE h_session);
+	void InitPin(CK_SESSION_HANDLE& h_session, CK_UTF8CHAR pin);
 
+	void OpenSession(CK_SLOT_ID slotID, CK_SESSION_HANDLE& h_session, CK_BYTE application);
+	void CloseSession(CK_SESSION_HANDLE& h_session);
+
+	void CreateObject(CK_SESSION_HANDLE& h_session, CK_OBJECT_HANDLE& h_object, std::vector<CK_ATTRIBUTE>& objTemplate);
+	void DestroyObject(CK_SESSION_HANDLE& h_session, CK_OBJECT_HANDLE& h_object);
 	
-
 	CK_FUNCTION_LIST_PTR GetFuncListPtr() {
 		return m_funcList;
 	}
@@ -104,9 +107,8 @@ void CryptoProvider::GetSlotCollection(CK_BBOOL tokenPresent, std::vector<CK_SLO
 
 }
 
-void CryptoProvider::OpenSession(CK_SLOT_ID slotID, CK_SESSION_HANDLE h_session) {
+void CryptoProvider::OpenSession(CK_SLOT_ID slotID, CK_SESSION_HANDLE& h_session, CK_BYTE application) {
 	CK_RV rv;
-	CK_BYTE application = 1;
 	
 	rv = m_funcList->C_OpenSession(slotID, CKF_SERIAL_SESSION | CKF_RW_SESSION, (CK_VOID_PTR)&application, NULL_PTR, &h_session);
 
@@ -114,7 +116,7 @@ void CryptoProvider::OpenSession(CK_SLOT_ID slotID, CK_SESSION_HANDLE h_session)
 		throw RetVal(rv);
 }
 
-void CryptoProvider::CloseSession(CK_SESSION_HANDLE h_session) {
+void CryptoProvider::CloseSession(CK_SESSION_HANDLE& h_session) {
 	CK_RV rv;
 
 	rv = m_funcList->C_CloseSession(h_session);
@@ -123,7 +125,32 @@ void CryptoProvider::CloseSession(CK_SESSION_HANDLE h_session) {
 		throw RetVal(rv);
 }
 
+void CryptoProvider::CreateObject(CK_SESSION_HANDLE& h_session, CK_OBJECT_HANDLE& h_object, std::vector<CK_ATTRIBUTE>& objTemplate)
+{
+	CK_RV rv;
+	rv = GetFuncListPtr()->C_CreateObject(h_session, &objTemplate[0], objTemplate.size(), &h_object);
 
+	if (rv != CKR_OK)
+		throw RetVal(rv);
+}
+
+void CryptoProvider::DestroyObject(CK_SESSION_HANDLE& h_session, CK_OBJECT_HANDLE& h_object) {
+	CK_RV rv;
+
+	rv = GetFuncListPtr()->C_DestroyObject(h_session, h_object);
+
+	if (rv != CKR_OK)
+		throw RetVal(rv);
+}
+
+void CryptoProvider::InitPin(CK_SESSION_HANDLE& h_session, CK_UTF8CHAR pin) {
+	CK_RV rv;
+
+	rv = GetFuncListPtr()->C_InitPIN(h_session, &pin, sizeof(pin) - 1);
+
+	if (rv != CKR_OK)
+		throw RetVal(rv);
+}
 
 CryptoProvider::~CryptoProvider() {
 	if (m_lib != NULL) {
@@ -189,39 +216,6 @@ public:
 
 
 
-class Object {
-private:
-	CryptoProvider* m_provider;
-
-public:
-	Object(CryptoProvider* provider) : m_provider(provider) { };
-	void CreateObject(CK_SESSION_HANDLE h_session, CK_OBJECT_HANDLE h_object, 
-		CK_OBJECT_CLASS objClass, std::vector<CK_ATTRIBUTE> objTemplate);
-	void DestroyObject(CK_SESSION_HANDLE h_session, CK_OBJECT_HANDLE h_object);
-};
-
-
-
-void Object::CreateObject(CK_SESSION_HANDLE h_session, CK_OBJECT_HANDLE h_object, 
-	CK_OBJECT_CLASS objClass, std::vector<CK_ATTRIBUTE> objTemplate) {
-
-	CK_RV rv;
-	rv = m_provider->GetFuncListPtr()->C_CreateObject(h_session, &objTemplate[0], objTemplate.size(), &h_object);
-
-	if (rv != CKR_OK)
-		throw RetVal(rv);
-}
-
-void Object::DestroyObject(CK_SESSION_HANDLE h_session, CK_OBJECT_HANDLE h_object) {
-	CK_RV rv;
-
-	rv = m_provider->GetFuncListPtr()->C_DestroyObject(h_session, h_object);
-
-	if (rv != CKR_OK)
-		throw RetVal(rv);
-}
-
-
 class key_AES {
 private:
 	CK_MECHANISM m_mechanism = {
@@ -231,7 +225,8 @@ private:
 
 public:
 	key_AES(CK_SESSION_HANDLE h_session, CK_OBJECT_HANDLE h_key, CryptoProvider* provider, 
-		std::vector<CK_ATTRIBUTE> objTemplate) : m_provider(provider) {
+		std::vector<CK_ATTRIBUTE> objTemplate) : m_provider(provider) 
+	{
 		CK_RV rv;
 		rv = m_provider->GetFuncListPtr()->C_GenerateKey(h_session, &m_mechanism, &objTemplate[0], objTemplate.size(), &h_key);
 
@@ -248,11 +243,40 @@ int main() {
 
 	CryptoProvider provider(L"D:\\SoftHSM2\\lib\\softhsm2-x64.dll");
 
-	provider.Initialize();
+	CK_BYTE app = 1;
+
+	CK_C_INITIALIZE_ARGS initArgs;
+
+	try {
+		provider.Initialize();
+	}
+	catch (LibLoadErr LibEx) {
+		std::cout << LibEx.what();
+		return LibEx.errcode();
+	}
+	catch (FuncListErr FuncListEx) {
+		std::cout << FuncListEx.what();
+		return FuncListEx.errcode();
+	}
+	catch (FuncLoadErr FuncLoadEx) {
+		std::cout << FuncLoadEx.what();
+		return FuncLoadEx.errcode();
+	}
+	catch (RetVal RetEx) {
+		std::cout << "Initialize\n";
+		std::cout << RetEx.what();
+		return RetEx.errcode();
+	}
 
 	std::vector<CK_SLOT_ID> slotCollection;
-	provider.GetSlotCollection(true, slotCollection);
-
+	try {
+		provider.GetSlotCollection(true, slotCollection);
+	}
+	catch (RetVal RetEx) {
+		std::cout << "GetSlotCollection\n";
+		std::cout << RetEx.what();
+		return RetEx.errcode();
+	}
 	std::vector<Token*> tokenCollection;
 	for (size_t i = 0; i < slotCollection.size(); ++i) {
 		Slot slot(slotCollection[i], &provider);
@@ -260,5 +284,73 @@ int main() {
 		tokenCollection.push_back(&token);
 	}
 
-	provider.Finalize();
+	PrintSlots(slotCollection);
+
+	CK_SESSION_HANDLE h_Session;
+
+	try {
+		provider.OpenSession(slotCollection[0], h_Session, app);
+	}
+	catch (RetVal RetEx) {
+		std::cout << "OpenSession\n";
+		std::cout << RetEx.what();
+		return RetEx.errcode();
+	}
+
+	CK_UTF8CHAR PIN[] = { "1234" };
+
+	provider.InitPin(h_Session, *PIN);
+
+
+	CK_OBJECT_HANDLE h_Key;
+
+	CK_OBJECT_CLASS KeyClass = CKO_SECRET_KEY;
+	CK_KEY_TYPE KeyType = CKK_AES;
+	CK_BBOOL True = CK_TRUE;
+	
+	std::vector<CK_ATTRIBUTE> keyTemplate
+	{
+		{CKA_CLASS, &KeyClass, sizeof(KeyClass)},
+		{CKA_KEY_TYPE, &KeyType, sizeof(KeyType)},
+		{CKA_WRAP, &True, sizeof(True)},
+		{CKA_UNWRAP, &True, sizeof(True)},
+		{CKA_ENCRYPT, &True, sizeof(True)},
+		{CKA_DECRYPT, &True, sizeof(True)},
+	};
+
+	try {
+		provider.CreateObject(h_Session, h_Key, keyTemplate);
+	}
+	catch (RetVal RetEx) {
+		std::cout << "CreateObject\n";
+		std::cout << RetEx.what();
+		return RetEx.errcode();
+	}
+
+	try {
+		provider.DestroyObject(h_Session, h_Key);
+	}
+	catch (RetVal RetEx) {
+		std::cout << "DestroyObject\n";
+		std::cout << RetEx.what();
+		return RetEx.errcode();
+	}
+
+	try {
+		provider.CloseSession(h_Session);
+	}
+	catch (RetVal RetEx) {
+		std::cout << "CloseSession\n";
+		std::cout << RetEx.what();
+		return RetEx.errcode();
+	}
+
+	try {
+		provider.Finalize();
+	}
+	catch (RetVal RetEx) {
+		std::cout << "Finalize\n";
+		std::cout << RetEx.what();
+		return RetEx.errcode();
+	}
 }
